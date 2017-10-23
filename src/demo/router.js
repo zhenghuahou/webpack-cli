c6a6651599e527324da5bd3e9a0edf367ad89935
侯正华 <2430370966@qq.com>
2017-10-23 20:01:03 +0800

    Add

--- src/demo/router.js
+++ src/demo/router.js
@@ -1,7 +1,11 @@
 //测试页面
-// const test = () => import(/* webpackChunkName: "test" */ './views/test') //ok
-const test = resolve => import(/* webpackChunkName: "test" */ './views/test').then(resolve);
+const test = () => import(/* webpackChunkName: "test" */ './views/test') //ok
+// const test = (resolve) => import(/* webpackChunkName: "test" */ './views/test').then(function(r){
+//   console.warn('resolve------------>',resolve,'  \n arguments------->',arguments,' \n r:',r,resolve === arguments[0],resolve === r);
+//   r.call(null,arguments[1]);
+// }.bind(this,resolve));
  
+// console.warn(' test:',test,' --->',resolve => import(/* webpackChunkName: "test" */ './views/test'))
 export default [{
     path: '/test',
     name: 'test',
