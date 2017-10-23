37ec8c1bd2d29f48be4b88064d459b55fce422c9
侯正华 <2430370966@qq.com>
2017-10-23 16:46:58 +0800

    add

--- config/mine.js
+++
@@ -1,9 +0,0 @@
-export default {
-    devServerPort: 8000, // 开发服务器端口号
-    bkdServerPort: 8001, // 后端服务器端口号
-    // proxyTarget: 'http://fe.iwjw.com:8888/api/fete_api/FF9SaW/d2TJHB/mock', // 代理目标API
-    proxyTarget: 'http://pc.iwjwtest.com',  // 代理目标API
-    // proxyTarget: 'http://pc.iwjwbeta.com',  // 代理目标API
-}
-
-
